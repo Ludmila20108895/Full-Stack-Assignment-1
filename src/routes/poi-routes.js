65efa9e7c995e9134ecdff2eb49365e7b0f6d81f
Ludmila20108895 <156093798+Ludmila20108895@users.noreply.github.com>
2025-03-22 00:22:48 +0000

    Update poi-routes.js

--- src/routes/poi-routes.js
+++ src/routes/poi-routes.js
@@ -2,11 +2,11 @@ import { poiController } from "../controllers/poi-controller.js";
 import { upload } from "../utils/upload-utils.js";
 
 export const poiRoutes = [
-  { method: "GET", path: "/pois", handler: poiController.listPois }, // ✅ Main POI List
-  { method: "GET", path: "/pois/add", handler: poiController.showAddPoiForm }, // ✅ Add Form
-  { method: "POST", path: "/pois", handler: poiController.addPoi }, // ✅ Add POI
-  { method: "GET", path: "/pois/{id}", handler: poiController.showPoi }, // ✅ View POI
-  { method: "POST", path: "/pois/delete/{id}", handler: poiController.deletePoi }, // ✅ Delete POI
+  { method: "GET", path: "/pois", handler: poiController.listPois }, //  Main POI List
+  { method: "GET", path: "/pois/add", handler: poiController.showAddPoiForm }, //  Add Form
+  { method: "POST", path: "/pois", handler: poiController.addPoi }, //  Add POI
+  { method: "GET", path: "/pois/{id}", handler: poiController.showPoi }, //  View POI
+  { method: "POST", path: "/pois/delete/{id}", handler: poiController.deletePoi }, //  Delete POI
   { method: "GET", path: "/added-places/{id}", handler: poiController.showAddedPlaces },
   {
     method: "POST",
