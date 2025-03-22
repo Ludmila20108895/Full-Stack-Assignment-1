abc9ce61ac3104a6fd47e51455883fa2a622f029
Ludmila20108895 <156093798+Ludmila20108895@users.noreply.github.com>
2025-03-22 00:23:44 +0000

    Update user-model.js

--- src/models/mongo/user-model.js
+++ src/models/mongo/user-model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }, // ✅ No longer hashed
+  password: { type: String, required: true }, //  No longer hashed
 });
 
 export const User = mongoose.model("User", userSchema);
