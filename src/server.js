d232988e8691aad1036963b7e619dbef052f051f
Ludmila20108895 <156093798+Ludmila20108895@users.noreply.github.com>
2025-03-28 04:40:51 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ dotenv.config(); // will load  environment variables from .env file
 // Will create the Hapi server instance
 const server = Hapi.server({
   port: process.env.PORT || 3000,
-  host: "localhost",
+  host: "0.0.o.o",   //we will need to deploy in Render
   routes: {
     files: {
       relativeTo: Path.join(process.cwd(), "public"), // Serve static files from /public
